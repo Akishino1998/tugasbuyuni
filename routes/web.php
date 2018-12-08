fc503a71e55f33111c64de3b76747cf08c276efe
Hendra97 <hendradarmawan5097@gmail.com>
2018-12-09 02:18:53 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,8 @@ Route::get('/lokasi-jemput-antar','HomeController@set_lokasi');
 
 // ===========================================================
 //Route Admin
-Route::get('/admin','AdminController@index');
+Route::get('/home-admin','AdminController@index');
+Route::get('home-login','AdminController@login');
+Route::get('home-register','AdminController@register');
+Route::get('data-user','AdminController@data_user');
+Route::get('data-customer','AdminController@data_customer')
